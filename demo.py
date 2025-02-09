552882cec8c1ca3256d52a35e16c5f06c35f6efc
Paco Nathan <paco@derwen.ai>
2025-02-09 14:51:47 -0800

    improve instructions

--- demo.py
+++ demo.py
@@ -7,23 +7,6 @@ see copyright/license <https://github.com/DerwenAI/kleptosyn/blob/main/LICENSE>
 Synthetic data generation for investigative graphs based on
 patterns of bad-actor tradecraft.
 
-This uses the following process:
-
-  1. Construct a _Network_ that represents bad-actor subgraphs
-    - Use `OpenSanctions` (risk data) and `OpenOwnership` (link data) for real-world UBO networks
-    - Run `Senzing` entity resolution to generate a "backbone" for organizing the graph
-    - Partition into subgraphs and run centrality measures
-
-  2. Configure a _Simulation_ for generating patterns of bad-actor tradecraft
-    - Analyze the network and transactions of the OCCRP "Azerbaijani Laundromat" leaked dataset (event data)
-    - Sample from probabilities distributions for shell company topologies, transfer amounts and timing
-    - Generate a large portion of "legit" transfers (49:1 ratio)
-
-  3. Generate the _SynData_ by applying the simulation on the network to generate synthetic data
-    - Serialize the transactions and people/companies involved
-    - Track the generated bad-actor transactions
-
-
 Steps (so far):
 
   * load the Senzing ER results for these datasets
